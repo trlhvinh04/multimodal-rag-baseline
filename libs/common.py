c7cfbc84059258096f5c4dad4a15aabb94fb3c78
dinhtanloc <locdinh.31221020226@st.ueh.edu.vn>
2025-05-16 01:59:43 +0700

    Feat: Dataset loading and Label generation

--- libs/common.py
+++ libs/common.py
@@ -17,3 +17,17 @@ from dotenv import load_dotenv,find_dotenv
 import pytesseract
 from datasets import load_dataset
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+
+import os
+import json
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_google_vertexai import ChatVertexAI
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from google.oauth2 import service_account
+import re
